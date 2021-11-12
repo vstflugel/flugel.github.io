a04b632906b2a5ce7168d59364bfd9fbf652de79
Flugel <vaibhav.tomar@students.iiit.ac.in>
2021-11-12 10:36:01 +0530

    rsync.sh function has been updated such that all linux user can use it and edit it according to their needs easily

--- rsync.sh
+++ rsync.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+
+# First we need to go to the root directory to copy the whole files for backing up
+
 cd ~ ;
 cd ..
 cd ..
+
+# then this is the command for copying the files and saving it to a desired location according to our will.
+
+
+# sudo rsync -aAXv  --delete   --exclude="$1" "$2"  "$3"
+# $1   Here add anything you wanted to exclude
+# $2   Here add the files or complete system directory which you wanted to backup
+# $3   Here add the location you wanted to save the files for backup.
+
 sudo rsync -aAXv  --delete   --exclude=/home/flugel/Downloads /home/flugel /media/flugel/Data/backups_rsync
